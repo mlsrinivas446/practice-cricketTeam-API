8a2e616ed36e1461e26a810b52a916ec5168713e
mlsrinivas446 <154888229+mlsrinivas446@users.noreply.github.com>
2024-02-02 18:53:54 +0530

    Update app.js
    cricket team

--- app.js
+++ app.js
@@ -5,85 +5,72 @@ const path = require('path')
 const app = express()
 app.use(express.json())
 
+const dbPath = path.join(__dirname, 'cricketMatchDetails.db')
 let db = null
-const dbPath = path.join(__dirname, 'cricketTeam.db')
 
-const initlaizeAndDbServer = async () => {
+const initializeDBAndServer = async () => {
   try {
     db = await open({
       filename: dbPath,
       driver: sqlite3.Database,
     })
     app.listen(3000, () => {
-      console.log('Server running at https://localhost:3000/')
+      console.log('Server running at http://localhost:3000/')
     })
   } catch (error) {
-    console.log(`DB error ${error.message}`)
+    console.log(`DB ERROR: ${error.message}`)
     process.exit(1)
   }
 }
-initlaizeAndDbServer()
-
-const camelCaseFunction = responseObj => {
-  //convet keys snake-case to camelCase
-  return {
-    playerId: responseObj.player_id,
-    playerName: responseObj.player_name,
-    jerseyNumber: responseObj.jersey_number,
-    role: responseObj.role,
-  }
-}
+initializeDBAndServer()
 
 app.get('/players/', async (request, response) => {
-  const getPlayerQuerys = `SELECT * FROM cricket_team ORDER BY player_id;`
-  const getPlayersArrayList = await db.all(getPlayerQuerys)
-  response.send(getPlayersArrayList.map(eachObj => camelCaseFunction(eachObj))) //for multiple obj we need to iterate
-})
-
-app.post('/players/', async (request, response) => {
-  const playerDetails = request.body
-
-  const {playerName, jerseyNumber, role} = playerDetails
-  const playerQuery = `INSERT INTO 
-                          cricket_team (player_name,jersey_number, role) 
-                       VALUES(
-                          '${playerName}',
-                          ${jerseyNumber},
-                          '${role}');`
-  const getPlayerDetails = await db.run(playerQuery)
-  const playerId = getPlayerDetails.lastID
-  response.send('Player Added to Team')
+  const playersQuery = `SELECT player_id AS playerId,player_name AS playerName FROM player_details`
+  const playerDetails = await db.all(playersQuery)
+  response.send(playerDetails)
 })
 
 app.get('/players/:playerId/', async (request, response) => {
   const {playerId} = request.params
-  const getplayerQuery = `SELECT * FROM cricket_team WHERE player_id=${playerId}`
-  const getPlayer = await db.get(getplayerQuery)
-  console.log(getPlayer)
-  response.send(camelCaseFunction(getPlayer)) //single obj no need to iterate
+  const playersQuery = `SELECT player_id AS playerId,player_name AS playerName FROM player_details WHERE player_id=${playerId}`
+  const playerDetails = await db.get(playersQuery)
+  response.send(playerDetails)
 })
 
 app.put('/players/:playerId/', async (request, response) => {
-  const playerDetails = request.body
   const {playerId} = request.params
-  const {playerName, jerseyNumber, role} = playerDetails
-  const updatePlayerQuery = `UPDATE
-                               cricket_team
-                            SET 
-                                player_name='${playerName}',
-                                jersey_number=${jerseyNumber} ,
-                                role= '${role} '
-                            WHERE 
-                                player_id=${playerId};`
-  await db.run(updatePlayerQuery)
+  const {playerName} = request.body
+  const playersQuery = `UPDATE player_details SET player_name='${playerName}' WHERE player_id=${playerId}`
+  const playerDetails = await db.get(playersQuery)
   response.send('Player Details Updated')
 })
 
-app.delete('/players/:playerId/', async (request, response) => {
+app.get('/matches/:matchId/', async (request, response) => {
+  const {matchId} = request.params
+  const playersQuery = `SELECT match_id AS matchId,match AS match,year AS year FROM match_details WHERE match_id=${matchId}`
+  const playerDetails = await db.get(playersQuery)
+  response.send(playerDetails)
+})
+
+app.get(`/players/:playerId/matches`, async (request, response) => {
   const {playerId} = request.params
-  const deleteQuery = `DELETE FROM cricket_team WHERE player_id=${playerId}`
-  await db.get(deleteQuery)
-  response.send('Player Removed')
+  const playersQuery = `SELECT match_id AS matchId,match AS match,year AS year FROM match_details NATURAL JOIN player_match_score  WHERE player_id=${playerId}`
+  const playerDetails = await db.all(playersQuery)
+  response.send(playerDetails)
+})
+
+app.get(`/matches/:matchId/players`, async (request, response) => {
+  const {matchId} = request.params
+  const playersQuery = `SELECT player_id AS playerId,player_name AS playerName FROM player_match_score NATURAL JOIN player_details  WHERE match_id=${matchId}`
+  const playerDetails = await db.all(playersQuery)
+  response.send(playerDetails)
+})
+
+app.get(`/players/:playerId/playerScores`, async (request, response) => {
+  const {playerId} = request.params
+  const playersQuery = `SELECT player_id AS playerId,player_name AS playerName,SUM(score) AS totalScore,SUM(fours) AS totalFours,SUM(sixes) AS totalSixes FROM player_match_score INNER JOIN player_details  WHERE player_id=${playerId};`
+  const playerDetails = await db.all(playersQuery)
+  response.send(playerDetails)
 })
 
 module.exports = app
